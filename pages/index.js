ef58433e4fd788a646a809656f96e10fb389b540
Gabriel Salvador <gbsalvador@gmail.com>
2025-03-25 21:04:07 +0000

    Adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Larissa, eu amo você. Se você me ama, dá uma risadinha! \o/</h1>
+  return <h1>Larissa, eu amo você. Se você me ama, dá uma risadinha! \o/</h1>;
 }
 
 export default Home;
