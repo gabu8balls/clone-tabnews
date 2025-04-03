97ed794afc3eec10b5e54d6113c29f44a4400847
Gabriel Salvador <gbsalvador@gmail.com>
2025-04-03 05:00:17 +0000

    altera a mensagem da index

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-  return <h1>Larissa, eu amo você. Se você me ama, dá uma risadinha! \o/</h1>;
+  let content = (
+    <h1>Larissa, eu amo você. Se você me ama, dá uma risadinha! \o/</h1>
+  );
+  content += <h2>O Boralão me ajudou.</h2>;
+  return content;
 }
 
 export default Home;
