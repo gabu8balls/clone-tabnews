8b7444c545ceda571ff4f81bd12fd7705535e8c2
Gabriel Salvador <gbsalvador@gmail.com>
2025-04-03 05:04:50 +0000

    corrige a mensagem da index

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 function Home() {
-  let content = (
-    <h1>Larissa, eu amo você. Se você me ama, dá uma risadinha! \o/</h1>
+  return (
+    <h1>
+      Larissa, eu amo você. Se você me ama, dá uma risadinha! O Boralão me
+      ajudou.
+    </h1>
   );
-  content += <h2>O Boralão me ajudou.</h2>;
-  return content;
 }
 
 export default Home;
